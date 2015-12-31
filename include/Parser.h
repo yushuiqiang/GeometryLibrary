3f8b1d32b15137939037aeda1de92039daea3b12
threepark <threepark@163.com>
2015-12-31 08:37:18 +0800

    Enhance Point Cloud Normal Calculation.

--- include/Parser.h
+++ include/Parser.h
@@ -14,10 +14,20 @@ namespace GPP
         Parser();
         ~Parser();
 
+        // fileName should contains the path name and fileName, like ../Models/PointCloud.asc or C:/Models/PointCloud.asc
+        // Support format: asc, obj
         static PointCloud* ImportPointCloud(std::string fileName);
+
+        // fileName should contains the path name and fileName, like ../Models/PointCloud.asc or C:/Models/PointCloud.asc
+        // Support format: obj, stl, off, ply
         static TriMesh*    ImportTriMesh(std::string fileName);
 
+        // fileName should contains the path name and fileName, like ../Models/PointCloud.asc or C:/Models/PointCloud.asc
+        // Support format: obj, ply
         static void ExportPointCloud(std::string fileName, const IPointCloud* pointCloud);
+
+        // fileName should contains the path name and fileName, like ../Models/PointCloud.asc or C:/Models/PointCloud.asc
+        // Support format: obj, stl, ply
         static void ExportTriMesh(std::string fileName, const ITriMesh* triMesh);
     };
 }
