f0ab8f4446f5db10565f988a8a74cddc1d7e4f8b
threepark <threepark@163.com>
2016-06-10 17:46:40 +0800

    Update

--- include/SimplifyMesh.h
+++ include/SimplifyMesh.h
@@ -19,5 +19,9 @@ namespace GPP
         // vertexFields: it could be color, texture coordinate....
         static ErrorCode QuadricSimplify(ITriMesh* triMesh, Int targetVertexCount, bool keepBoundary = true, 
             const std::vector<Real>* vertexFields = NULL, std::vector<Real>* simplifiedVertexFields = NULL);
+
+        static ErrorCode SimplifyWithTextureCoords(ITriMesh* triMesh, Int targetVertexCount, 
+            const std::vector<Vector3>* texCoords, std::vector<Vector3>* simplifiedTexCoords, 
+            const std::vector<Real>* vertexFields = NULL, std::vector<Real>* simplifiedVertexFields = NULL);
     };
 }
