edc4606ef1e37a2ddc87ce8965e324f6afbb8d97
threepark <threepark@163.com>
2016-07-16 16:04:59 +0800

    Update

--- include/ToolPointCloud.h
+++ include/ToolPointCloud.h
@@ -7,6 +7,7 @@
 #pragma once
 #include "PointCloud.h"
 #include "IPointList.h"
+#include "Matrix4x4.h"
 #include <vector>
 
 namespace GPP
@@ -17,5 +18,11 @@ namespace GPP
 
     extern GPP_EXPORT ErrorCode CalculatePointListBoundingBox(const IPointList* pointlist, Vector3& bboxMin, Vector3& bboxMax);
 
+    extern GPP_EXPORT ErrorCode CalculatePointCloudListBoundingBox(const std::vector<IPointCloud*> pointCloudList, 
+        const std::vector<Matrix4x4>* transformList, Vector3& bboxMin, Vector3& bboxMax);
+
     extern GPP_EXPORT ErrorCode CalculatePointListDensity(const IPointList* pointList, Int neighborCount, Real& density);
+
+    // pointCloud should have normal, and this function just update normal and the orientation is the same as before
+    extern GPP_EXPORT ErrorCode UpdatePointCloudNormal(IPointCloud* pointCloud, Int neighborCount);
 }
