aa4f4923546d7c91899e964bdc9a1202680425b1
threepark <liguocn@qq.com>
2016-10-19 08:12:00 +0800

    update

--- include/IntrinsicColor.h
+++ include/IntrinsicColor.h
@@ -6,7 +6,7 @@
 ====================================================================================================*/
 #pragma once
 #include "IPointCloud.h"
-#include "PointCloud.h"
+#include "ITriMesh.h"
 #include "Matrix4x4.h"
 #include <vector>
 
@@ -27,19 +27,23 @@ namespace GPP
         ~IntrinsicColor();
 
         // neighborCount: range >= 4, usually be 12, larger value will make color fusing more
-        // cloudIds: the original point cloud id of point
         // pointColors: range[0, 1]
         static ErrorCode TuneColorFromMultiFrame(const IPointCloud* pointCloud, Int neighborCount, 
             const std::vector<Int>& colorIds, std::vector<Vector3>& pointColors);
 
         // Point cloud should have been aligned before calling this api
         // color value should be in range [0, 1]
+        // pointDensity: if it is NULL, it will be calculated automatically. If pointClouds are not uniformed, please set it manually
         static ErrorCode TuneColorFromSingleLight(const std::vector<IPointCloud*>& pointCloudList, 
-            std::vector<std::vector<Vector3> >& colorList, bool needBlend, std::vector<std::vector<Int> >* colorIdList);
+            std::vector<std::vector<Vector3> >& colorList, bool needBlend, const Real* pointDensity,
+            const std::vector<std::vector<Int> >* originColorIdList, std::vector<std::vector<Int> >* tunedColorIdList);
 
-        
+
         static ErrorCode TuneTextureImageByVertexColor(const std::vector<Real>& textureCoords, const std::vector<Vector3>& vertexColors, 
             Int imageWidth, Int imageHeight, const std::vector<PixelType> pixelTypes, std::vector<Vector3>& textureColors);
 
+
+        static ErrorCode TuneMeshColorFromMultiPatch(const ITriMesh* triMesh, const std::vector<Int>& colorIds, 
+            std::vector<Vector3>& vertexColors);
     };
 }
