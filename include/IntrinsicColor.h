96c83072293879d3e96f565ee67c9672fdc2d4e8
threepark <liguocn@qq.com>
2016-12-22 20:39:15 +0800

    update

--- include/IntrinsicColor.h
+++ include/IntrinsicColor.h
@@ -27,14 +27,30 @@ namespace GPP
         IntrinsicColor();
         ~IntrinsicColor();
 
+        // rgb range [0, 1]
+        // hsv range: h-[0, 360], s-[0, 1], v-[0, 1]
+        // H(hue), S(saturation), V(value)
+        static Vector3 ConvertRGB2HSV(const Vector3& rgb);
+
+        static Vector3 ConvertHSV2RGB(const Vector3& hsv);
+
         // Overview: pointColors will be merged into one consistent region by diffusing boundaries of different colorIds
         // pointCloud: it should be uniform. If not, please use TuneMeshColorFromMultiPatch instead
         // neighborCount: range >= 4, usually be 12, larger value will make color fusing more
         // colorIds: points having the same colorId should have consistent color
-        // pointColors: range[0, 1]
-        // sharpColorDiff: rangge[0, sqrt(3)]. Point color will be fixed on colorId boundary if color difference between neighbors is larger than sharpColorDiff
+        // pointColors: range[0, 1], should be in rgb space
+        // sharpColorDiff: range[0, 1]. Point color component will be fixed on colorId boundary if color difference between neighbors is larger than sharpColorDiff
+        // sharpColorDiff component meaning: 0 - H(hue), 1 - S(saturation), 2 - V(value)
         static ErrorCode TuneColorFromMultiFrame(const IPointCloud* pointCloud, Int neighborCount, 
-            const std::vector<Int>& colorIds, std::vector<Vector3>& pointColors, Real sharpColorDiff = 0.8);
+            const std::vector<Int>& colorIds, std::vector<Vector3>& pointColors, const Vector3& sharpColorDiff = Vector3(0.1, 1.0, 0.5));
+
+
+        // colorIds: vertices having the same colorId should have consistent color
+        // vertexColors: range[0, 1], should be in rgb space
+        // sharpColorDiff: range[0, 1]. Point color component will be fixed on colorId boundary if color difference between neighbors is larger than sharpColorDiff
+        // sharpColorDiff component meaning: 0 - H(hue), 1 - S(saturation), 2 - V(value)
+        static ErrorCode TuneMeshColorFromMultiPatch(const ITriMesh* triMesh, const std::vector<Int>& colorIds, 
+            std::vector<Vector3>& vertexColors, const Vector3& sharpColorDiff = Vector3(0.1, 1.0, 0.5));
 
         
         // Point cloud should have been aligned before calling this api
@@ -68,12 +84,6 @@ namespace GPP
         // pointCoords: point's coordinates in image space: x0 y0 x1 y1 ......
         static ErrorCode TuneImageByPointColor(const std::vector<Int>& pointCoords, const std::vector<Vector3>& pointColors,
             Int imageWidth, Int imageHeight, std::vector<Color4>& imageColors);
-
-
-        // colorIds: vertices having the same colorId should have consistent color
-        // vertexColors: range[0, 1]
-        static ErrorCode TuneMeshColorFromMultiPatch(const ITriMesh* triMesh, const std::vector<Int>& colorIds, 
-            std::vector<Vector3>& vertexColors);
 
     };
 }
